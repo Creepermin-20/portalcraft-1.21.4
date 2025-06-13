88f6ebc1514fefac8f4889f7f1d6cd8de49dccf7
Daniel Elmore <elmoreaydan101@gmail.com>
2025-06-13 14:09:40 -0500

    Initial commit

---
+++ src/main/java/com/portalcraft/PortalCraft.java
@@ -0,0 +1,16 @@
+package com.portalcraft;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PortalCraft implements ModInitializer {
+	public static final String MOD_ID = "portalcraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/com/portalcraft/PortalCraftClient.java
@@ -0,0 +1,10 @@
+package com.portalcraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PortalCraftClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
